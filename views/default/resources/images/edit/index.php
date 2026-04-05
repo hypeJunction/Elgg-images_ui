97d1bf34322737d7aa219e1846080f225a879b87
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-05 13:38:45 +0200

    migrate(3.x): automated AST transformations
    Applied by elgg-migrate automated rules.
    See elgg-migrate rules/2x-to-3x/manifest.json for details.

--- views/default/resources/images/edit/index.php
+++ views/default/resources/images/edit/index.php
@@ -2,53 +2,34 @@
 
 $guid = elgg_extract('guid', $vars);
 $entity = get_entity($guid);
-
 if (!images()->isImage($entity)) {
-	forward('', '404');
+    forward('', '404');
 }
-
 if (!$entity->canEdit()) {
-	forward('', '403');
+    forward('', '403');
 }
-
 $container = $entity->getContainerEntity();
-
 elgg_set_page_owner_guid($container->guid);
-
-elgg_group_gatekeeper();
-
+elgg_entity_gatekeeper();
 elgg_push_breadcrumb(elgg_echo('images'), '/images/all');
 if ($container) {
-	elgg_push_breadcrumb($container->getDisplayName(), "/images/all/$entity->container_guid");
+    elgg_push_breadcrumb($container->getDisplayName(), "/images/all/{$entity->container_guid}");
 }
 elgg_push_breadcrumb($entity->getDisplayName(), $entity->getURL());
-
 $title = elgg_echo('images:edit');
 elgg_push_breadcrumb($title);
-
 if (elgg_is_sticky_form('images/upload')) {
-	$sticky_values = elgg_get_sticky_values('images/upload');
-	if (is_array($sticky_values)) {
-		$vars = array_merge($vars, $sticky_values);
-	}
+    $sticky_values = elgg_get_sticky_values('images/upload');
+    if (is_array($sticky_values)) {
+        $vars = array_merge($vars, $sticky_values);
+    }
 }
-
 $vars['filter_context'] = 'index';
 $vars['entity'] = $entity;
-
-$content = elgg_view_form('images/upload', [
-	'enctype' => 'multipart/form-data',
-	'validate' => true,
-		], $vars);
-
+$content = elgg_view_form('images/upload', ['enctype' => 'multipart/form-data', 'validate' => true], $vars);
 if (elgg_is_xhr()) {
-	echo $content;
+    echo $content;
 } else {
-	$body = elgg_view_layout('content', [
-		'content' => $content,
-		'title' => $title,
-		'filter' => elgg_view('filters/images/edit', $vars),
-	]);
+    $body = elgg_view_layout('content', ['content' => $content, 'title' => $title, 'filter' => elgg_view('filters/images/edit', $vars)]);
 }
-
 echo elgg_view_page($title, $body);
