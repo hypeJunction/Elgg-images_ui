80c4bcb0301aaf5be79d1ebdb62c3bccb4023a87
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-05-11 10:14:24 +0200

    fix(6.x): lightbox loading → elgg_import_esm + elgg_load_external_file

--- views/default/file/specialcontent/image/default.php
+++ views/default/file/specialcontent/image/default.php
@@ -12,8 +12,8 @@ if (!$full || !$file instanceof ElggFile) {
 	return;
 }
 
-elgg_load_js('lightbox');
-elgg_load_css('lightbox');
+elgg_import_esm('elgg/lightbox');
+elgg_load_external_file('css', 'lightbox');
 
 $img = elgg_view('output/img', [
 	'alt' => $file->getDisplayName(),
